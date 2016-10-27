0c7e20a67c927de6fa76c1d55044c1dd5b9d797b
CrazySteven <956995511@qq.com>
2016-10-27 22:20:44 +0800

    ok

---
+++ UIScrollView_AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollView_AutoLayout
+//
+//  Created by 郭艾超 on 16/10/27.
+//  Copyright © 2016年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollView_AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollView_AutoLayout
+//
+//  Created by 郭艾超 on 16/10/27.
+//  Copyright © 2016年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
